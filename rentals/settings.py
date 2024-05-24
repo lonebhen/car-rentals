a95c27b7a22ccc1256a8e157d134b1bf706facfb
lonebhen <ben.angmortey@gmail.com>
2024-05-24 09:01:02 +0000

    [PROD] update build script

--- rentals/settings.py
+++ rentals/settings.py
@@ -189,8 +189,6 @@ if not DEBUG:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# MEDIA_URL = '/media/'  
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
