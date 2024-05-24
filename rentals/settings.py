514b52087117fb2e3932104b207e7a0c57027261
lonebhen <ben.angmortey@gmail.com>
2024-05-24 08:17:23 +0000

    [PROD] handle static files

--- rentals/settings.py
+++ rentals/settings.py
@@ -184,6 +184,10 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # MEDIA_URL = '/media/'  
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
