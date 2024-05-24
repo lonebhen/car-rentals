a93378f4be4ac58973c2ca331654c877a489b664
lonebhen <ben.angmortey@gmail.com>
2024-05-24 09:10:59 +0000

    [Update] build script

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+python manage.py makemigrations accounts
+python manage.py makemigrations core
+python manage.py makemigrations 
 python manage.py migrate
